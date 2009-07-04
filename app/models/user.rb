33f9cb40a7150a8c2344f8816ad507a6b3cdbf60
Caio Tiago Oliveira <caio@dcc.ufba.br>
2009-07-03 22:31:07 -0300

    Configurando user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
-class User < ActiveRecord::Base
+class User < ActiveLdap::Base
+  ldap_mapping  :dn_attribute => "uid", :prefix => "ou=people", :classes => ['top', 'pessoa']
+
+  def empty
+    self.have_attribute?("dn")
+  end
 end
