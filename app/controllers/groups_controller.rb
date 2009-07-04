761c1938aa72f5e92b85004fc4c648eaeb6f9dfc
Caio Tiago Oliveira <caio@dcc.ufba.br>
2009-07-03 21:03:55 -0300

    Adicionando o requisito de não incluir dois grupos de mesmo nome

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,9 +3,15 @@ class GroupsController < ApplicationController
     @groups = Group.find(:all)
   end
   def new
-    @group = Group.new(params[:group])
-    if @group.save
-      redirect_to :action => 'index'
+    if params[:group]
+      if Group.find(:filter => {:cn => params[:group][:cn]})
+        flash[:notice] = "<p>Já existe grupo com este nome.<br>Por favor, escolha outro.</p>"
+        redirect_to :action => "new"
+      end
+      @group = Group.new(params[:group])
+      if @group.save
+        redirect_to :action => 'index'
+      end
     end
   end
 end
