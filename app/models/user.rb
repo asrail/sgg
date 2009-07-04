923aadff572b563363eeb5297b0c083467619b5e
Caio Tiago Oliveira <caio@dcc.ufba.br>
2009-07-04 00:03:40 -0300

    Algumas melhorias para o user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveLdap::Base
-  ldap_mapping  :dn_attribute => "uid", :prefix => "ou=people", :classes => ['top', 'pessoa']
+  ldap_mapping  :dn_attribute => "uid", :prefix => "ou=people", :classes => ['top', 'person', 'shadowAccount', 'simpleSecurityObject']
+  belongs_to :groups, :class_name => 'Group', :many => 'memberUid'
 
   def empty
     self.have_attribute?("dn")
