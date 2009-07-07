0a8e78d506e3daf0ae41fd4f463dd292ca7f949b
Raoni Boaventura <raonidonordeste@gmail.com>
2009-07-07 14:03:27 -0300

    Adicionar usuário.(Sem administrador)

--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class WelcomeControllerTest < ActionController::TestCase
+class WelcomeControllerTest < Test::Unit::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+#  test "the truth" do
+#    assert true
+#  end
 end
