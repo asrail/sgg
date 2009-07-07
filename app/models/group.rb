c7703b3bf1ce2f56b958ec34d7819aeebf796957
Caio Tiago Oliveira <asrail@gmail.com>
2009-07-07 13:33:39 -0300

    Adicionando marshal_load e dump

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,12 @@
 class Group < ActiveLdap::Base
   ldap_mapping :dn_attribute => 'cn', :prefix => 'ou=Groups', :classes => ['top', 'posixGroup']
   has_many :members, :class_name => "User", :wrap => "memberUid"
+
+  def marshal_load(str)
+    load(str)
+  end
+  
+  def marshal_dump
+    dump
+  end
 end
