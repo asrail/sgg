324627c303210284da5f17459af79c4e0cdfb549
Caio Tiago Oliveira <caio@dcc.ufba.br>
2009-07-06 22:30:50 -0300

    Modificando o controller login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,2 +1,21 @@
 class LoginController < ApplicationController
+  skip_before_filter :authenticate
+  def index
+  end
+
+  def login
+    if session[:person] = LdapUser.authenticate(params[:login][:name], params[:login][:password])
+      session[:username] = params[:login][:name]
+      if session[:return_to]
+        redirect_to(session[:return_to])
+        session[:return_to] = nil
+      else
+        #XXXasrail: testar com :back
+        #redirect_to :controller => "default"
+      end
+    else
+      flash[:notice] = "Senha ou usuário incorretos"
+      redirect_to :action => "index"
+    end
+  end
 end
