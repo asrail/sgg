97ae0c6195c14f0985c8ed50c6675ec147076ac7
Caio Tiago Oliveira <caio@dcc.ufba.br>
2009-07-09 21:15:32 -0300

    Suportando coord. em group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveLdap::Base
-  ldap_mapping :dn_attribute => 'cn', :prefix => 'ou=Groups', :classes => ['top', 'posixGroup']
+  ldap_mapping :dn_attribute => 'cn', :prefix => 'ou=Groups', :classes => ['top', 'posixGroup', 'sggGroup']
   has_many :members, :class_name => "User", :wrap => "memberUid"
+  has_many :coordinators, :class_name => "User", :wrap => "coordinatorUid"
 
   def marshal_load(str)
     load(str)
