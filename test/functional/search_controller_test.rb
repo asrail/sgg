3e34d62f0f705efaed7e1f7232bc454c0d86d89b
Raoni Boaventura <raonidonordeste@gmail.com>
2009-07-21 14:47:05 -0300

    Atualizando testes

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+#  test "the truth" do
+#    assert true
+#  end
 end
