642ff1567db35f1e46af5a09ae04f8b07b938324
Raoni Boaventura <raonidonordeste@gmail.com>
2009-07-21 23:08:55 +0800

    Teste de integração de login
    Signed-off-by: Caio Tiago Oliveira <asrail@gmail.com>

--- test/integration/login_controller_test.rb
+++ test/integration/login_controller_test.rb
@@ -1,17 +1,20 @@
 require 'test_helper'
 
-class LoginControllerTest < Test::Unit::TestCase
+class LoginControllerTest < ActionController::IntegrationTest
+
+  def setup
+    User.new({:cn => "Raoni", :sn => "Boaventura", :userPassword => "blibli", :uid => "raoni"}).save
+  end
 
   # Replace this with your real tests.
     def test_logar
 	    get '/'
-	    assert_tag :tag => 'a', :attributes => { :href => '/login' }
+            assert_redirected_to '/login'
 
-	    get '/login'
 	    assert_tag :tag => 'form', :attributes => { :action => 'login', :method => 'post' }
 
 	    post '/login', :login => { :name => "raoni", :password => "blibli" }
-	    assert_redirected_to '/groups' 
+	    assert_redirected_to '/' 
     end
 
 end
