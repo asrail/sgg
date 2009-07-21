756218036407759b44ddb1c6952b3f744e04afb8
Caio Tiago Oliveira <caio@dcc.ufba.br>
2009-07-21 02:39:38 -0300

    Removendo método não utilizado

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveLdap::Base
   ldap_mapping  :dn_attribute => "uid", :prefix => "ou=Users", :classes => ['top', 'person', 'shadowAccount', 'simpleSecurityObject']
   belongs_to :groups, :class_name => 'Group', :many => 'memberUid'
 
-  def empty
-    self.have_attribute?("dn")
-  end
-
   def self.authenticate(username, password)
     return false if username.empty? or password.empty?
     find(username).bind(password)
